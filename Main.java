1cc60ce31dbfa1b5d198a5552fc787007eb1d63b
balez4464 <balez4464@wrdsb.ca>
2020-10-13 13:33:16 +0000

    Created a program that uses a method to figure out the factors of a number

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
 /**
- *
- * @author 
+ *This program uses a method to find out the facogrs of a given number
+ * @author Zachary Balean
  */
 public class Main {
+
+  //method for finding factors
+  public static void factors(int numberToBeDivided){
+    //creates variable for answers
+    int answer = 0;
+    //for loop to divide by factors
+    for(int i = 1; i <= numberToBeDivided; i++){
+      answer = numberToBeDivided % i;
+      //prints if a factor
+      if(answer == 0){
+        //prints factors
+        System.out.println(i);
+      }
+    }
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // calls on method
+    factors(10);
     
   }
 }
